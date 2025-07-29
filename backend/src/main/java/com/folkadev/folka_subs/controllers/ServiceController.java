b4ab9562e02c1085e7ecd8458cae1259f94c4739
folka2134 <folka2134@gmail.com>
2025-07-29 13:33:55 +0100

    feat: implemented service controller

---
+++ backend/src/main/java/com/folkadev/folka_subs/controllers/ServiceController.java
@@ -0,0 +1,59 @@
+package com.folkadev.folka_subs.controllers;
+
+import com.folkadev.folka_subs.domain.dto.ServiceDto;
+import com.folkadev.folka_subs.services.ServiceService;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/service")
+public class ServiceController {
+
+  private final ServiceService serviceService;
+
+  public ServiceController(ServiceService serviceService) {
+    this.serviceService = serviceService;
+  }
+
+  @GetMapping
+  public List<ServiceDto> getAllServices() {
+    return serviceService.getServices();
+  }
+
+  @GetMapping(path = "/{service_id}")
+  public ServiceDto getService(@RequestParam("service_id") UUID serviceId) {
+    return serviceService.getService(serviceId);
+  }
+
+  @PostMapping()
+  @ResponseStatus(HttpStatus.CREATED)
+  public ResponseEntity<ServiceDto> createService(@RequestBody ServiceDto serviceDto) {
+
+    ServiceDto createdServiceDto = serviceService.createService(serviceDto);
+    return new ResponseEntity<>(createdServiceDto, HttpStatus.CREATED);
+  }
+
+  @PutMapping(path = "/{service_id}")
+  public ServiceDto updateService(@PathVariable("service_id") UUID serviceId, @RequestBody ServiceDto serviceDto) {
+    return serviceService.updateService(serviceId, serviceDto);
+  }
+
+  @DeleteMapping(path = "/{service_id}")
+  public void deleteService(@PathVariable("service_id") UUID serviceId) {
+    serviceService.deleteService(serviceId);
+  }
+}
