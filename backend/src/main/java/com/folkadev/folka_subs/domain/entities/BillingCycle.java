552b87619a44edfd10b932a3259bfca70a5cb8c4
folka2134 <folka2134@gmail.com>
2025-07-29 11:15:08 +0100

    chore: Set up enums for Subscription entity

---
+++ backend/src/main/java/com/folkadev/folka_subs/domain/entities/BillingCycle.java
@@ -0,0 +1,5 @@
+package com.folkadev.folka_subs.domain.entities;
+
+public enum BillingCycle {
+  WEEKLY, MONTHLY, QUARTERLY, YEARLY
+}
---
+++ backend/src/main/java/com/folkadev/folka_subs/domain/entities/Status.java
@@ -0,0 +1,5 @@
+package com.folkadev.folka_subs.domain.entities;
+
+public enum Status {
+  ACTIVE, EXPIRED, CANCELED, TRIAL
+}
