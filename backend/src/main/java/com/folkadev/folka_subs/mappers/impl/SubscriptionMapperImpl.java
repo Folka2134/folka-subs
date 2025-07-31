e478c2107c9cc4a9749dc3528c342f5441393109
folka2134 <folka2134@gmail.com>
2025-07-31 11:23:51 +0100

    chore: updated subscription to store service

--- backend/src/main/java/com/folkadev/folka_subs/mappers/impl/SubscriptionMapperImpl.java
+++ backend/src/main/java/com/folkadev/folka_subs/mappers/impl/SubscriptionMapperImpl.java
@@ -3,14 +3,25 @@ package com.folkadev.folka_subs.mappers.impl;
 import org.springframework.stereotype.Component;
 
 import com.folkadev.folka_subs.domain.dto.SubscriptionDto;
+import com.folkadev.folka_subs.domain.entities.Service;
 import com.folkadev.folka_subs.domain.entities.Subscription;
 import com.folkadev.folka_subs.mappers.SubscriptionMapper;
+import com.folkadev.folka_subs.repositories.ServiceRepository;
 
 @Component
 public class SubscriptionMapperImpl implements SubscriptionMapper {
+
+  private final ServiceRepository serviceRepository;
+
+  public SubscriptionMapperImpl(ServiceRepository serviceRepository) {
+    this.serviceRepository = serviceRepository;
+  }
+
   @Override
   public Subscription fromDto(SubscriptionDto subscriptionDto) {
-    return new Subscription(subscriptionDto.serviceId(), subscriptionDto.price(), subscriptionDto.currency(),
+    Service service = serviceRepository.findById(subscriptionDto.serviceId())
+        .orElseThrow(() -> new RuntimeException("Service not found"));
+    return new Subscription(service, subscriptionDto.price(), subscriptionDto.currency(),
         subscriptionDto.trialPeriodDays(), subscriptionDto.billingCycle(), subscriptionDto.notificationDaysBefore(),
         subscriptionDto.status(), subscriptionDto.notes(), subscriptionDto.startDate(),
         subscriptionDto.cancelledDate());
@@ -18,7 +29,7 @@ public class SubscriptionMapperImpl implements SubscriptionMapper {
 
   @Override
   public SubscriptionDto toDto(Subscription subscription) {
-    return new SubscriptionDto(subscription.getId(), subscription.getServiceId(), subscription.getPrice(),
+    return new SubscriptionDto(subscription.getId(), subscription.getService().getId(), subscription.getPrice(),
         subscription.getCurrency(), subscription.getTrialPeriodDays(), subscription.getBillingCycle(),
         subscription.getNotificationDaysBefore(), subscription.getStatus(), subscription.getNotes(),
         subscription.getStartDate(), subscription.getCancelledDate());
