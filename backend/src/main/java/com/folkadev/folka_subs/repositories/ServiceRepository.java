4c9dce4d9474f48ddafaa1bd96f388257d25bedd
folka2134 <folka2134@gmail.com>
2025-07-31 11:25:39 +0100

    chore: service repository method now uneeded

--- backend/src/main/java/com/folkadev/folka_subs/repositories/ServiceRepository.java
+++ backend/src/main/java/com/folkadev/folka_subs/repositories/ServiceRepository.java
@@ -1,16 +1,12 @@
 package com.folkadev.folka_subs.repositories;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.folkadev.folka_subs.domain.entities.Service;
-import com.folkadev.folka_subs.domain.entities.Subscription;
 
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, UUID> {
-
-  List<Subscription> findSubscriptionsByServiceId(UUID serviceId);
 }
