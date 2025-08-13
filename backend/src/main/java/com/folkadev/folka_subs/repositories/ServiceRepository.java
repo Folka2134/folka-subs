ccc6350d99a30b2af3ea7436ff3587967c9913e3
folka2134 <folka2134@gmail.com>
2025-08-13 17:56:37 +0100

    feat: added find service by name

--- backend/src/main/java/com/folkadev/folka_subs/repositories/ServiceRepository.java
+++ backend/src/main/java/com/folkadev/folka_subs/repositories/ServiceRepository.java
@@ -1,12 +1,15 @@
 package com.folkadev.folka_subs.repositories;
 
+import java.util.Optional;
+import com.folkadev.folka_subs.domain.entities.Service;
+
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.folkadev.folka_subs.domain.entities.Service;
-
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, UUID> {
+
+  Optional<Service> findByName(String name);
 }
